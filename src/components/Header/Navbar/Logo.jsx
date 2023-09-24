6e4d9b18392710288f59200a85c11027dee5356b
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-09-25 00:54:07 +0600

    created navbar with active route

---
+++ src/components/Header/Navbar/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <img src="images/Logo.png" alt="" />
+        </div>
+    );
+};
+
+export default Logo;
