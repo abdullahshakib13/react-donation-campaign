fef8e2c557432beaeebae1178673158de4348338
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-09-25 23:16:22 +0600

    blog file added in donation page

--- src/Pages/Blog/BlogCard.jsx
+++ src/Pages/Blog/BlogCard.jsx
@@ -4,29 +4,52 @@ const BlogCard = ({ blog }) => {
     const { id, image, title, category } = blog || {};
 
     const handleDonate = () => {
-        localStorage.setItem('first','shakib')
+        const addedDonationarray = [];
+
+        const donateItems = JSON.parse(localStorage.getItem('donation'));
+
+        if(!donateItems){
+            addedDonationarray.push(blog);
+            localStorage.setItem('donation', JSON.stringify(addedDonationarray))
+            alert('products added');
+        } else {
+
+            const isExist = donateItems.find(blog => blog.id == id);
+
+            if (!isExist) {
+                addedDonationarray.push(...donateItems, blog);
+                localStorage.setItem('donation', JSON.stringify(addedDonationarray));
+                alert('products added');
+            } else {
+                alert('already ase')
+            }
+
+
+            
+        }
+        // localStorage.setItem('first','shakib')
     }
     return (
         <div className="flex justify-center items-center">
-            <div class="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-            <div class="relative mx-4 -mt-6 h-56 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+            <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <div className="relative mx-4 -mt-6 h-56 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
                 <img
                 src={image}
                 alt="img-blur-shadow"
                 layout="fill"
                 />
             </div>
-            <div class="p-6">
-                <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            <div className="p-6">
+                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
                {title}
                 </h5>
-                <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
                 There are many things that can be done to ensure that all people have access to a good education. Governments can invest in public schools, provide financial assistance to students, and make sure that all schools have qualified teachers and resources. Families can support their children's education by creating a learning environment at home and helping them with their schoolwork. Teachers can create a positive and supportive learning environment for their students and challenge them to reach their full potential.
                 </p>
             </div>
-            <div class="p-6 pt-0">
+            <div className="p-6 pt-0">
                 <button onClick={handleDonate}
-                class="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
                 data-ripple-light="true"
                 >
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import BlogCard from "../Blog/BlogCard";
+
 const Donation = () => {
+
+    const [donation, setDonation] = useState();
+    
+    const [notFound, setNotFound] = useState('');
+    useEffect(() => {
+        const donateItems = JSON.parse(localStorage.getItem('donation'));
+        if (donateItems) {
+            
+            setDonation(donateItems);
+        } else {
+          setNotFound('no data found')  
+        }
+    }, [])
+    console.log(donation);
     return (
         <div>
-            <h2>Donation</h2>
+            {notFound ? <p className="flex justify-center items-center">{notFound}</p> :
+                <div>
+                    <div className="grid grid-cols-1 lg:grid-cols-2">
+                        {
+                            donation?.map(blog=> <BlogCard key={blog.id} blog={blog}></BlogCard>)
+                        }
+                    </div>
+                </div>}
         </div>
     );
 };
