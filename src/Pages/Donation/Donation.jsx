d66d32f8969ca1c4a4924e75ff66ebd739008ed5
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-09-26 00:08:17 +0600

    created see all button in donation page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -6,6 +6,9 @@ const Donation = () => {
     const [donation, setDonation] = useState();
     
     const [notFound, setNotFound] = useState('');
+
+    const [isShow, setIsShow] = useState(false);
+
     useEffect(() => {
         const donateItems = JSON.parse(localStorage.getItem('donation'));
         if (donateItems) {
@@ -15,16 +18,24 @@ const Donation = () => {
           setNotFound('no data found')  
         }
     }, [])
-    console.log(donation);
+    // console.log(donation);
     return (
         <div>
             {notFound ? <p className="flex justify-center items-center">{notFound}</p> :
                 <div>
                     <div className="grid grid-cols-1 lg:grid-cols-2">
                         {
-                            donation?.map(blog=> <BlogCard key={blog.id} blog={blog}></BlogCard>)
+                            isShow ? donation?.map(blog => <BlogCard key={blog.id} blog={blog}></BlogCard>) : 
+                                donation?.slice(0,4).map(blog=> <BlogCard key={blog.id} blog={blog}></BlogCard>)
                         }
                     </div>
+                    {/* {donation.length > 4 && <button onClick={() => setIsShow(!isShow)}
+                        className="px-5 bg-green-200 block mx-auto">
+                        {isShow ? 'See less' : 'See All'}</button>} */}
+                    <button onClick={() => setIsShow(!isShow)}
+                        className="px-5 bg-green-200 block mx-auto">
+                        {isShow ? 'See less' : 'See All'}</button>}
+                    
                 </div>}
         </div>
     );
